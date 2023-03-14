d6b28f3fa293e35591351058224414cbf00692aa
andanyang <1218853253@qq.com>
2023-03-14 18:01:13 +0800

    baiduPush

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://andanyang.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.admin4j.com&token=2cNRvC6KVmu00hDN"
 
 rm -rf urls.txt # 删除文件
